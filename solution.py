4654fe35cbb1f68a4666813761aa6e2851205f0b
cheklinim <ivan.cheklin.1985@gmail.com>
2020-11-27 10:56:04 +0300

    Add fizz_buzz() function into solution.py

--- solution.py
+++ solution.py
@@ -17,6 +17,20 @@ def binary_sum(a, b):
     return bin(a + b)[2:]
 
 
+def fizz_buzz(begin, end):
+    result = []
+    for i in range(begin, end + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append('FizzBuzz')
+        elif i % 3 == 0:
+            result.append('Fizz')
+        elif i % 5 == 0:
+            result.append('Buzz')
+        else:
+            result.append(str(i))
+    return ' '.join(result)
+
+
 if __name__ == "__main__":
     """
     Модуль содержит функции-решения задач на Hexlet.io по специализации Python-программист.
